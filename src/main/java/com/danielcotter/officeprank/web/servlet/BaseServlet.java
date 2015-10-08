74e9f79fa1db06355c1b39e0e143faeacb1fe921
Daniel Cotter <daniel@proctorfree.com>
2015-10-08 16:00:21 -0400

    Unarmed by default on startup

--- src/main/java/com/danielcotter/officeprank/web/servlet/BaseServlet.java
+++ src/main/java/com/danielcotter/officeprank/web/servlet/BaseServlet.java
@@ -12,5 +12,5 @@ public class BaseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1163127896685573599L;
 	protected static final ObjectMapper mapper = new ObjectMapper();
 	protected static List<String> alreadyHashed = new ArrayList<String>();
-	protected static Boolean armed = true;
+	protected static Boolean armed = false;
 }
